bddc4155ccee3fc820c0f57e373f10658279ff5f
Akinsanmi Timothy <timwebdev147@gmail.com>
2022-11-07 13:12:15 +0100

    changes

--- api/index.php
+++ api/index.php
@@ -67,7 +67,7 @@ $port = 587;
 $bodyText =  "Hey Bankress \r\n".$message;
 
 // The HTML-formatted body of the email
-$bodyHtml = "<h1>Message from client (".$fullname.")</h1>
+$bodyHtml = "<h1 style='color: black;'>Message from client (".$fullname.")</h1>
 <p>".$message." <br/>
 client's email: ".$email." <br/>
 client's phone number: ".$phone."
